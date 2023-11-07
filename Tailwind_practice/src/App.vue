cffd521826b29d0f08365c9fd1e342b8199010bd
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-11-07 18:13:08 +0545

    Tailwind_practice

---
+++ Tailwind_practice/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import tailwind from './pages/tailwind.vue';
+</script>
+
+<template>
+ 
+<tailwind/>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
